84493e36587b9e8e23412f42baf19b0ad4b4c78f
Min Kang <kangm914@gmail.com>
2019-05-09 18:20:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,29 +2,9 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts "   |   |   "
+  puts "board[0]   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-
